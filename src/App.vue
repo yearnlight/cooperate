fdde572e5e9cd9a261d074f6ad60a5748880c613
yearnlight <291190413@qq.com>
2019-08-13 11:03:54 +0800

    协作平台

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+/*!
+*虎虎虎 
+*2019-3-1618:03:56 
+*yangmx
+*/
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style lang="less">
+#app {
+  height: 100%;
+}
+
+.el-button {
+  border-radius: 0;
+}
+</style>
